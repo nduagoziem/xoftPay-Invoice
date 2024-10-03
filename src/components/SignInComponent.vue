c8517c2ccef4e68a97100423a3a6a91b1f7f439a
nduagoziem <162841062+nduagoziem@users.noreply.github.com>
2024-10-03 10:00:36 +0100

    register and login components

---
+++ src/components/SignInComponent.vue
@@ -0,0 +1,44 @@
+<script setup>
+    import { ref } from 'vue';
+    import InputGroup from 'primevue/inputgroup';
+    import InputGroupAddon from 'primevue/inputgroupaddon';
+    import InputText from 'primevue/inputtext';
+    import Password from 'primevue/password';
+
+    const value = ref(null);
+</script>
+
+<template>
+    <form action="">
+
+        <div>
+
+            <InputGroup class="mt-4 mb-4 py-2">
+                <InputGroupAddon>
+                    <span class="pi pi-user-edit"></span>
+                </InputGroupAddon>
+                <InputText placeholder="Username" required/>
+            </InputGroup>
+
+            <InputGroup  class="input mb-4 py-2">
+                <InputGroupAddon>
+                    <span class="pi pi-lock"></span>
+                </InputGroupAddon>
+                <Password v-model="value" toggleMask :feedback="false" placeholder="Password" required/>
+            </InputGroup>
+
+            <button type="submit" class="py-2 text-center">Login</button>
+        </div>
+    </form>
+</template>
+
+<style scoped>
+    button {
+        background-color: black;
+        width: 100%;
+        border: none;
+        border-radius: 3px;
+        outline: none;
+        color: rgb(123, 183, 235);
+    }
+</style>
---
+++ src/components/SignUpComponent.vue
@@ -0,0 +1,57 @@
+<script setup>
+    import { ref } from 'vue';
+    import InputGroup from 'primevue/inputgroup';
+    import InputGroupAddon from 'primevue/inputgroupaddon';
+    import InputText from 'primevue/inputtext';
+    import Password from 'primevue/password';
+
+    const value = ref(null);
+</script>
+
+<template>
+    <form action="">
+
+        <div>
+            <InputGroup class="mt-4 mb-4 py-2">
+                <InputGroupAddon>
+                    <span class="pi pi-user"></span>
+                </InputGroupAddon>
+                <InputText placeholder="Full Name" required/>
+            </InputGroup>
+
+            <InputGroup class="mt-4 mb-4 py-2">
+                <InputGroupAddon>
+                    <span class="pi pi-user-edit"></span>
+                </InputGroupAddon>
+                <InputText placeholder="Username" required/>
+            </InputGroup>
+
+            <InputGroup  class="input mb-4 py-2">
+                <InputGroupAddon>
+                    <span class="pi pi-lock"></span>
+                </InputGroupAddon>
+                <Password v-model="value" toggleMask :feedback="false" placeholder="Password" required/>
+            </InputGroup>
+
+            <InputGroup  class="input mb-4 py-2">
+                <InputGroupAddon>
+                    <span class="pi pi-lock"></span>
+                </InputGroupAddon>
+                <Password v-model="value" toggleMask :feedback="false" placeholder="Confirm Password" required/>
+            </InputGroup>
+        </div>
+
+        <button type="submit" class="py-2 text-center">Register</button>
+    </form>
+</template>
+
+<style scoped>
+    button {
+        background-color: black;
+        width: 100%;
+        border: none;
+        border-radius: 3px;
+        outline: none;
+        color: rgb(123, 183, 235);
+    }
+</style>
