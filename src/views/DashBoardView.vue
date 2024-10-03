7ca13df50929b182754e6f51057266b898f894f1
nduagoziem <162841062+nduagoziem@users.noreply.github.com>
2024-10-03 09:56:45 +0100

    deleted Dashboard.vue and changed it to DashBoardVew

---
+++ src/views/DashBoardView.vue
@@ -0,0 +1,21 @@
+<script setup>
+    import GreetingComponent from '@/components/GreetingComponent.vue';
+    import TotalInvoicesReport from '@/components/TotalInvoicesReport.vue';
+    import CreateInvoiceButton from '@/components/CreateInvoiceButton.vue';
+    import SalesExpensesChart from '@/components/SalesExpensesChart.vue';
+    import RecentInvoice from '@/components/RecentInvoice.vue';
+    import ProfitMarginChart from '@/components/ProfitMarginChart.vue';
+</script>
+
+<template>
+    <GreetingComponent />
+    <TotalInvoicesReport />
+    <CreateInvoiceButton />
+    <SalesExpensesChart />
+    <RecentInvoice />
+    <ProfitMarginChart />
+</template>
+
+<style scoped>
+    
+</style>
