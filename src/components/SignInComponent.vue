1d2eea66dd98396648416ca9af29796e36d88032
nduagoziem <162841062+nduagoziem@users.noreply.github.com>
2024-10-05 11:55:26 +0100

    update

--- src/components/SignInComponent.vue
+++ src/components/SignInComponent.vue
@@ -1,87 +1,86 @@
 <script setup>
-    import { ref } from 'vue';
-    import InputGroup from 'primevue/inputgroup';
-    import InputGroupAddon from 'primevue/inputgroupaddon';
-    import InputText from 'primevue/inputtext';
-    import Password from 'primevue/password';
-    import router from '@/router';
-    // import axios from 'axios';
-    import { loginUrl } from '@/config/api';
-    import { useToast } from 'vue-toastification';
+import { ref } from "vue";
+import InputGroup from "primevue/inputgroup";
+import InputGroupAddon from "primevue/inputgroupaddon";
+import InputText from "primevue/inputtext";
+import Password from "primevue/password";
+import router from "@/router";
+import axios from 'axios';
+import { loginUrl } from "@/config/api";
+import { useToast } from "vue-toastification";
 
-    const username = ref("");
-    const password = ref("");
+const username = ref("");
+const password = ref("");
 
-    const toast = useToast()
+const toast = useToast();
 
-    async function loginUser() {
+async function loginUser() {
+  try {
+    const res = await axios.post(`${loginUrl}`,
+        
+        {
+            username: username.value,
+            password: password.value
+        },
+        {
+            headers: {
+                'Content-Type': 'application/json',
+                withCredentials: false,
+            },
+        },
+    );
 
-        try {
+    const result = await res.json();
 
-            const res = await fetch(`${loginUrl}`, 
-            {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json',
-                    },
-                    body: JSON.stringify(
-                        {
-                            username: username.value,
-                            password: password.value
-                        }
-                    )
-                }
-            );
+    if (result.data.success) {
+      username.value = "";
+      password.value = "";
 
-            const result = await res.json()
-            
-            if (result.data.success) {
-                username.value = "";
-                password.value = "";
-
-                router.push('/dashboard');
-            } 
-            else {
-                toast("Login failed");
-            }
-        }
-        catch(err) {
-            console.error(err)
-        }
+      router.push("/dashboard");
+    } else {
+      toast("Login failed");
     }
+  } catch (err) {
+    console.error(err);
+  }
+}
 </script>
 
 <template>
-    <form @submit.prevent="loginUser">
+  <form @submit.prevent="loginUser">
+    <div>
+      <InputGroup class="mt-4 mb-4 py-2">
+        <InputGroupAddon>
+          <span class="pi pi-user-edit"></span>
+        </InputGroupAddon>
+        <InputText v-model="username" placeholder="Username" required />
+      </InputGroup>
 
-        <div>
+      <InputGroup class="input mb-4 py-2">
+        <InputGroupAddon>
+          <span class="pi pi-lock"></span>
+        </InputGroupAddon>
+        <Password
+          v-model="password"
+          toggleMask
+          :feedback="false"
+          placeholder="Password"
+          required
+        />
+      </InputGroup>
 
-            <InputGroup class="mt-4 mb-4 py-2">
-                <InputGroupAddon>
-                    <span class="pi pi-user-edit"></span>
-                </InputGroupAddon>
-                <InputText v-model="username" placeholder="Username" required/>
-            </InputGroup>
-
-            <InputGroup  class="input mb-4 py-2">
-                <InputGroupAddon>
-                    <span class="pi pi-lock"></span>
-                </InputGroupAddon>
-                <Password v-model="password" toggleMask :feedback="false" placeholder="Password" required/>
-            </InputGroup>
-
-            <button type="submit" class="py-2 text-center">Login</button>
-        </div>
-    </form>
+      <button type="submit" class="py-2 text-center">Login</button>
+    </div>
+  </form>
 </template>
 
 <style scoped>
-    button {
-        background-color: black;
-        width: 100%;
-        border: none;
-        border-radius: 3px;
-        outline: none;
-        color: rgb(123, 183, 235);
-    }
+button {
+  background-color: black;
+  width: 100%;
+  border: none;
+  border-radius: 3px;
+  outline: none;
+  color: rgb(123, 183, 235);
+}
 </style>
--- src/components/SignUpComponent.vue
+++ src/components/SignUpComponent.vue
@@ -1,107 +1,114 @@
 <script setup>
-    import { ref } from 'vue';
-    import InputGroup from 'primevue/inputgroup';
-    import InputGroupAddon from 'primevue/inputgroupaddon';
-    import InputText from 'primevue/inputtext';
-    import Password from 'primevue/password';
-    // import axios from 'axios';
-    import { registerUrl } from '@/config/api';
-    import router from '@/router';
-    import { useToast } from 'vue-toastification';
+import { ref } from "vue";
+import InputGroup from "primevue/inputgroup";
+import InputGroupAddon from "primevue/inputgroupaddon";
+import InputText from "primevue/inputtext";
+import Password from "primevue/password";
+import axios from "axios";
+import { registerUrl } from "@/config/api";
+import router from "@/router";
+import { useToast } from "vue-toastification";
 
-    const fullName = ref("")
-    const userName = ref("")
-    const passwordValue = ref(null);
-    const confirmPasswordValue = ref(null);
+const fullName = ref("");
+const userName = ref("");
+const passwordValue = ref(null);
+const confirmPasswordValue = ref(null);
 
-    const toast = useToast()
+const toast = useToast();
 
-    const registerData = {
-        fullName: fullName,
-        userName: userName,
-        passwordValue: passwordValue,
-        confirmPasswordValue: confirmPasswordValue,
+const registerData = {
+  fullName: fullName,
+  userName: userName,
+  passwordValue: passwordValue,
+  confirmPasswordValue: confirmPasswordValue,
+};
+
+async function registerUser() {
+  try {
+    const res = await axios.post(`${registerUrl}`,
+        
+        registerData,
+        {
+            headers: {
+                'Content-Type': 'application/json',
+                withCredentials: false,
+            },
+        },
+    );
+
+    if (res.data.success) {
+      fullName.value = "";
+      userName.value = "";
+      passwordValue.value = "";
+      confirmPasswordValue.value = "";
+
+      router.push("/dashboard");
+    } else {
+      toast.error("Something went wrong, Try again");
     }
 
-    async function registerUser() {
-        try {
-            const res = await fetch(`${registerUrl}`, 
-                {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json',
-                    },
-                    body: JSON.stringify(
-                        {registerData}
-                    )
-                }
-
-            );
-
-            const result = await res.json()
-
-            if (result.data.success) {
-                fullName.value = "";
-                userName.value = "";
-                passwordValue.value = "";
-                confirmPasswordValue.value = "";
-
-                router.push("/dashboard");
-            } 
-            else {
-                toast.error("Something went wrong, Try again"); 
-            }
-            
-            return res;
-        }
-        catch (err) {console.error(err)}
-    } 
+    return res;
+  } catch (err) {
+    console.log(err);
+  }
+}
 </script>
 
 <template>
-    <form @submit.prevent="registerUser">
+  <form @submit.prevent="registerUser">
+    <div>
+      <InputGroup class="mt-4 mb-4 py-2">
+        <InputGroupAddon>
+          <span class="pi pi-user"></span>
+        </InputGroupAddon>
+        <InputText placeholder="Full Name" required v-model="fullName" />
+      </InputGroup>
 
-        <div>
-            <InputGroup class="mt-4 mb-4 py-2">
-                <InputGroupAddon>
-                    <span class="pi pi-user"></span>
-                </InputGroupAddon>
-                <InputText placeholder="Full Name" required v-model="fullName"/>
-            </InputGroup>
+      <InputGroup class="mt-4 mb-4 py-2">
+        <InputGroupAddon>
+          <span class="pi pi-user-edit"></span>
+        </InputGroupAddon>
+        <InputText placeholder="Username" require v-model="userName" />
+      </InputGroup>
 
-            <InputGroup class="mt-4 mb-4 py-2">
-                <InputGroupAddon>
-                    <span class="pi pi-user-edit"></span>
-                </InputGroupAddon>
-                <InputText placeholder="Username" require v-model="userName"/>
-            </InputGroup>
+      <InputGroup class="input mb-4 py-2">
+        <InputGroupAddon>
+          <span class="pi pi-lock"></span>
+        </InputGroupAddon>
+        <Password
+          v-model="passwordValue"
+          toggleMask
+          :feedback="false"
+          placeholder="Password"
+          required
+        />
+      </InputGroup>
 
-            <InputGroup  class="input mb-4 py-2">
-                <InputGroupAddon>
-                    <span class="pi pi-lock"></span>
-                </InputGroupAddon>
-                <Password v-model="passwordValue" toggleMask :feedback="false" placeholder="Password" required/>
-            </InputGroup>
+      <InputGroup class="input mb-4 py-2">
+        <InputGroupAddon>
+          <span class="pi pi-lock"></span>
+        </InputGroupAddon>
+        <Password
+          v-model="confirmPasswordValue"
+          toggleMask
+          :feedback="false"
+          placeholder="Confirm Password"
+          required
+        />
+      </InputGroup>
+    </div>
 
-            <InputGroup  class="input mb-4 py-2">
-                <InputGroupAddon>
-                    <span class="pi pi-lock"></span>
-                </InputGroupAddon>
-                <Password v-model="confirmPasswordValue" toggleMask :feedback="false" placeholder="Confirm Password" required/>
-            </InputGroup>
-        </div>
-
-        <button type="submit" class="py-2 text-center">Register</button>
-    </form>
+    <button type="submit" class="py-2 text-center">Register</button>
+  </form>
 </template>
 
 <style scoped>
-    button {
-        background-color: black;
-        width: 100%;
-        border: none;
-        border-radius: 3px;
-        outline: none;
-        color: rgb(123, 183, 235);
-    }
+button {
+  background-color: black;
+  width: 100%;
+  border: none;
+  border-radius: 3px;
+  outline: none;
+  color: rgb(123, 183, 235);
+}
 </style>
